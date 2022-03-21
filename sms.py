f887f3e4764333bafc2a6e81a0383762a97b253f
Bad Boy <67397663+sr326201@users.noreply.github.com>
2022-03-21 03:37:25 +0330

    Create sms.py

---
+++ sms.py
@@ -0,0 +1,8 @@
+import os
+os.system('apt update')
+os.system('apt upgrade')
+os.system('pip install colorama')
+os.system('pip install requests')
+os.system('pip install time')
+os.system('clear')
+os.system('python x.py')
